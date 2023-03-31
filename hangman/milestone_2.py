109aaa1c9c87c44622d32f7fbd9bc94065404c96
Kristina Gorkovskaya <kristina.gorkovskaya@gmail.com>
2023-03-31 09:56:08 +0100

    Updates milestone2 code to use isalpha() instead of regex to validate user input

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,11 +1,10 @@
 import random
-import re
 
 word_list = ["papaya", "pineapple", "pomegranate"]
 word = random.choice(word_list)
 
 guess = input('Enter a letter: ').strip().lower()
-if re.match('^[a-z]$', guess):
+if len(guess) == 1 and guess.isalpha():
     print('Good guess!')
 else:
     print('Oops! That is not a valid input.')
